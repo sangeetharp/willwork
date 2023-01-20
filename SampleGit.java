108091dcebcb70f68c783eaf9710d830d885a3e0
sangeetharp <sangitha14694@gmail.com>
2023-01-20 21:27:29 +0530

    1st commit

--- SampleGit.java
+++ SampleGit.java
@@ -1,5 +1,10 @@
 package org.giit;
 
-public class SampleGit {
+public class SampleGit { 
+	public static void main(String[] args) {
+	
+
   System.out.println("from repository(github)");
+  System.out.println("clone succesful");
+}
 }
