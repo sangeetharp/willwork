61890c55203c136f162ba1aa758ad1378533beed
sangeetharp <sangitha14694@gmail.com>
2023-01-21 16:54:59 +0530

    5th commit

--- SampleGit.java
+++ SampleGit.java
@@ -1,14 +1,15 @@
 package org.giit;
 
-public class SampleGit { 
+public class SampleGit {
 	public static void main(String[] args) {
 	
 
   System.out.println("from repository(github)");
   System.out.println("clone succesful");
   System.out.println("after clonning");
-
-		system.out.println("from 2 repository");
+System.out.println("from 2 repository");
 System.out.println("conflict resolved");
+
+System.out.println("saturday");
 
 }
