161da8463d00e9aca0a8e108f30b8684e978a29c
sangeetharp <107559748+sangeetharp@users.noreply.github.com>
2023-02-07 21:34:09 +0530

    Update SampleGit.java

--- SampleGit.java
+++ SampleGit.java
@@ -4,19 +4,6 @@ public class SampleGit {
 	public static void main(String[] args) {
 	
 
-  System.out.println("from repository(github)");
-  System.out.println("clone succesful");
-  System.out.println("after clonning");
-System.out.println("conflict resolved");
-System.out.println("akka nalarukingala");
-System.out.println("saptingala ka");
-System.out.println("udamba paathukonga ka");
-		System.out.println("working fine");
-
-		System.out.println("i did a change")
-		
-
-System.out.println("saturday");
-System.out.println("feb");
+ 
 
 }
